95b515b56b2ea64f89d936e6fd0ce713cfb08850
monichaudharyx-prog <monichaudharyx@gmail.com>
2026-05-11 22:17:56 +0530

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,4 @@
+/** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  serverBuildPath: "build/index.js",
-  publicPath: "/build/"
+  ignoredRouteFiles: ["**/.*"]
 };
