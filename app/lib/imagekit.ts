81b250e866cc2721d55ddf4ee88f56d154739f5a
monichaudharyx-prog <monichaudharyx@gmail.com>
2026-05-11 20:38:28 +0530

    Update imagekit.ts

--- app/lib/imagekit.ts
+++ app/lib/imagekit.ts
@@ -1,12 +1,11 @@
 export async function uploadImage(file: File) {
   const formData = new FormData();
+
   formData.append("file", file);
+  formData.append("publicKey", process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || "");
 
   const res = await fetch("https://upload.imagekit.io/api/v1/files/upload", {
     method: "POST",
-    headers: {
-      Authorization: "Basic YOUR_IMAGEKIT_AUTH"
-    },
     body: formData
   });
 
